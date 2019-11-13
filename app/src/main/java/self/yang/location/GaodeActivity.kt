319b2a496a100169d914050cd74c63d052cb6fc8
yangguoqing <ysh1059@163.com>
2019-11-13 12:49:23 +0800

    增加高德地图

--- app/src/main/java/self/yang/location/GaodeActivity.kt
+++ app/src/main/java/self/yang/location/GaodeActivity.kt
@@ -1,11 +1,13 @@
 package self.yang.location
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.amap.api.location.AMapLocation
 import com.amap.api.location.AMapLocationClient
 import com.amap.api.location.AMapLocationListener
+import com.amap.api.maps.MapView
 
 
 class GaodeActivity : AppCompatActivity() {
@@ -13,6 +15,8 @@ class GaodeActivity : AppCompatActivity() {
     //声明AMapLocationClient类对象
     var mLocationClient: AMapLocationClient? = null
     var mLocationListener: AMapLocationListener? = null
+    //高德地图视图
+    var mMapView: MapView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +40,39 @@ class GaodeActivity : AppCompatActivity() {
         mLocationClient!!.setLocationListener(mLocationListener);
 
         mLocationClient!!.startLocation()
+
+        //获取地图控件引用
+        mMapView = findViewById<MapView>(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView?.onCreate(savedInstanceState)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        mLocationClient?.unRegisterLocationListener(mLocationListener)
+
+        mMapView?.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView?.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView?.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+
+        mMapView?.onSaveInstanceState(outState)
     }
 
     // 展示位置信息
